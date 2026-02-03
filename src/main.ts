07ab80488f8e618ca2ab5e997cde64dd379a5d1b
yileifeng <fengyilei1@gmail.com>
2026-02-03 11:35:43 -0500

    setup i18n and FR canada.ca page

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
+import { i18n } from './lang';
 import './style.css'
 // import 'ramp-pcar/dist/ramp.css';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(i18n).mount('#app')
---
+++ src/shims-vue.d.ts
@@ -0,0 +1,10 @@
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
+
+declare module '*lang.csv' {
+    import { LocaleMessages, VueMessageType } from 'vue-i18n';
+    const content: LocaleMessages<VueMessageType>;
+    export default content;
+}
