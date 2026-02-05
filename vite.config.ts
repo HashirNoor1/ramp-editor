3f7b644024788d6318489d920c93fd08305ef26a
yileifeng <fengyilei1@gmail.com>
2026-02-05 10:01:22 -0500

    update Vite build to include FR html page

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';=
 import vue from '@vitejs/plugin-vue';
 import dsv from '@rollup/plugin-dsv';
 
@@ -10,6 +10,13 @@ export default defineConfig({
     build: {
         target: 'esnext',
         commonjsOptions: { transformMixedEsModules: true },
-        rollupOptions: { external: ['ramp-pcar'] }
+        rollupOptions: {
+            input: {
+                main: '/index.html',
+                en: '/index.html',
+                fr: '/index-fr.html'
+            },
+            external: ['ramp-pcar']
+        }
     }
 });
